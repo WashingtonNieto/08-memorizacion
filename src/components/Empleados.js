686d56eb83f0816247c7a817ccf5c1b7ae3c9241
Washington Nieto <washington_nieto@hotmail.com>
2025-02-04 09:57:22 -0500

    primer ejemplo de memo

--- src/components/Empleados.js
+++ src/components/Empleados.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export const Empleados = () => {
-  return (
-    <div>Empleados</div>
-  )
-}
+export const Empleados = React.memo(
+  ({pagina}) => {
+
+    const [empleados, setEmpleados] = useState([]);
+
+    useEffect(() => {
+      conseguirEmpleados(pagina);
+    }, [pagina]);
+
+    const conseguirEmpleados = async(p) => {
+      const url = "https://reqres.in/api/users?page="+p;
+      const peticion = await (fetch(url));
+      const {data:empleados} = await peticion.json();
+
+      setEmpleados(empleados);
+    }
+
+    return (
+      <div>
+        <p>Mostrando la pagina:{pagina}</p>
+        <ul className='empleados'>
+          {empleados.length >=1 && empleados.map(empleado => {
+            return <li key={empleado.id}>{empleado.first_name + " " +empleado.last_name}</li>
+          })}
+        </ul>
+      </div>
+    );
+  }
+);
--- src/components/Gestion.js
+++ src/components/Gestion.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { Empleados } from './Empleados'
 
 export const Gestion = () => {
+
+  const [ nombre, setNombre ] = useState("");
+  const [ pagina, setPagina ] = useState(1);
+
+  const asignarGestor = e => {
+    setNombre(e.target.value);
+  }
+
+
   return (
     <div>
-      <h1>Nombre del gestor : Washington</h1>
+      <h1>Nombre del gestor : {nombre}</h1>
+
+      <input type="text" onChange={asignarGestor} placeholder="Introduce nombre gestor" />
 
       <h2>Listado de empleados</h2>
-      <p>Los usuarios vienen de </p>
+      <p>Los usuarios son gestionados por {nombre} </p>
+      <button onClick={() => { setPagina(1) }}>Página 1</button> 
+      <button onClick={() => { setPagina(2) }}>Página 2</button>
+
+      <Empleados pagina={pagina} />
+
     </div>
   )
 }
