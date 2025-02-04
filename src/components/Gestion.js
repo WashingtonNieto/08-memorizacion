820c899bf470907b909442c4fc024143448f433e
Washington Nieto <washington_nieto@hotmail.com>
2025-02-04 07:37:04 -0500

    Inicio

---
+++ src/components/Gestion.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Gestion = () => {
+  return (
+    <div>
+      <h1>Nombre del gestor : Washington</h1>
+
+      <h2>Listado de empleados</h2>
+      <p>Los usuarios vienen de </p>
+    </div>
+  )
+}
