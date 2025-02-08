a6d32511296f967f231af0b8277bcbb6e72f0f1c
Washington Nieto <washington_nieto@hotmail.com>
2025-02-08 11:01:10 -0500

    Formulario añadir y borrar

--- src/components/Tareas.js
+++ src/components/Tareas.js
@@ -1,16 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Tareas = () => {
+
+  const [tareas, setTareas]= useState([]);
+
+  const guardarTareas  = e => {
+    e.preventDefault();
+
+    setTareas(tarea => [...tareas, e.target.descripcion.value]);
+  }
+
+  const borrarTarea = id => {
+    //Filtar las tareas
+    let nuevas_tareas = tareas.filter((tarea, indice) => indice !== id);
+    // console.log(nuevas_tareas);
+
+    //Restablece el estado
+    setTareas(nuevas_tareas);
+  }
+
+
   return (
+
     <div className='tareas-container'>
       <h1>Tareas</h1>
-      <form>
+      <form onSubmit={guardarTareas}>
         <input type="text" name="descripcion" placeholder='Describe la tara' />
         <input type="submit" values="Guardar" />
       </form>
 
       <h3>Lista de tareas</h3>
-      
+      <ul>
+        {
+          tareas.map((tarea, indice) => {
+            return( 
+              <li key={indice}>
+                {tarea}
+                &nbsp;
+                <button onClick={() => borrarTarea(indice)}>X</button>
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
   )
 }
